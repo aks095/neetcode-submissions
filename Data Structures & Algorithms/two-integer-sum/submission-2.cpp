78761490e2c3ff548a193644cbaf669184fd40e1
aks095 <95340386+aks095@users.noreply.github.com>
2026-07-08 22:35:08 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0;
+        map <int, int> m;
+        for(int i = 0; i < nums.size(); i++){
+            if(m[target - nums[i]] > 0)return {m[target-nums[i]] - 1, i};
+            m[nums[i]] = i + 1;
+        }
+
+        return {};
+
+
+        
+        
+
+        
+    }
+};
